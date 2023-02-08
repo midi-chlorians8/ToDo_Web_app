2eb5aebf5d37c93efa0139ee9a0d009d2b4e5248
iu <ilya.romanovskiy.2015@mail.ru>
2023-02-09 02:59:10 +0300

    init

--- main.py
+++ main.py
@@ -11,7 +11,13 @@ class Item(BaseModel):
 
 app = FastAPI()
 
+# Блок кода чтобы считать с сервера
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
 
+# Блок кода чтобы отправить на сервер
 @app.post("/items/")
 async def create_item(item: Item):
     return item
+
