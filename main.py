69dff2964a7ad509bc4a0ab9208c004028bd4b99
iu <ilya.romanovskiy.2015@mail.ru>
2023-02-19 15:43:02 +0300

    add

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from dotenv import load_dotenv
+# from pathlib import Path
+import os
+# dotenv_path = Path('.env')
+load_dotenv()
+
+
 # DataBase
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -5,8 +12,9 @@ from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost/postgres"
-
+SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL') #"postgresql://postgres:postgres@localhost/postgres" #os.getenv('SQLALCHEMY_DATABASE_URI') #"postgresql://postgres:postgres@localhost/postgres"
+print(type(os.getenv('SQLALCHEMY_DATABASE_URL')))
+print(type("postgresql://user:password@postgresserver/db"))
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
@@ -27,9 +35,10 @@ from pydantic import BaseModel
 class Note(Base):
     __tablename__ = "notes"
      
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True) # id заметки. Незнаю пока наскок нада эта колонка
     name = Column(String, unique=True, index=True)
     description = Column(String)
+    users_id = Column(Integer) #Какому юзеру принадлежит заметка
 # Models
 
 Base.metadata.create_all(bind=engine)
@@ -50,8 +59,12 @@ class Note_Pydant(BaseModel):
     id: int
     name: str
     description: str | None = None
-    
 
+
+# notedict = {
+#   "name": "Holo",
+#   "description": "avc"
+# }
 
 
 @app.post("/send_note/")
@@ -78,6 +91,35 @@ def delete_note(note_id: int):
     return {"ok": True}
 
 
+
+
+
+# Вывод страницы с заметками 
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, Request
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/notes/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    read_notes = db.query(Note).offset(1).limit(10).all()
+    return templates.TemplateResponse("item.html", {"request": request, "id": read_notes})
+
+class Command(BaseModel):
+    command: str
+
+@app.post("/process_command")
+async def process_command(command: Command):
+    # replace this with the actual code to process the command
+    print(f"Received command: {command.command}")
+    return {"status": "Command received and processed successfully"}
+# Вывод страницы с заметками 
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8001)  # run app on the host and port
    
+
+
