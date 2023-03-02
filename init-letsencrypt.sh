d55b3972b1b88585b2ab3d11acce252c1faedc4a
iu <ilya.romanovskiy.2015@mail.ru>
2023-03-02 20:34:07 +0300

    add

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(march.kolotech.space)
+domains=(marchpd.kolotech.space)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="dobryjsok60@gmail.com" # Adding a valid address is strongly recommended
